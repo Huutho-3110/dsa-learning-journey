79e08f109793c9ccfa6a0c893c94d10654374c28
Nguyen Huu Tho <tho.nguyenhuu3110@hcmut.edu.vn>
2026-08-10 14:50:25 +0700

    algo: giving another solutions for ex4 les7

--- problems/tech28/L07_E4_prime_digits/prime_digits.cpp
+++ problems/tech28/L07_E4_prime_digits/prime_digits.cpp
@@ -21,44 +21,96 @@
 #include <math.h>
 using namespace std;
 
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n == 2 || n == 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+bool isDigit(int n)
+{
+    while (n)
+    {
+        int temp = n % 10;
+        if (temp != 2 && temp != 3 && temp != 5 && temp != 7)
+            return false;
+        temp /= 10;
+    }
+    return true;
+}
+
+void solve1()
+{
+    long long a, b;
+    cin >> a >> b;
+    int cnt = 0;
+    vector<bool> isPrime(b + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (long long i = 2; i * i <= b; i++)
+    {
+        if (!isPrime[i])
+            continue;
+        for (long long j = i * i; j <= b; j += i)
+        {
+            isPrime[j] = false;
+        }
+    }
+    for (long long i = a; i <= b; i++)
+    {
+        if (!isPrime[i])
+            continue;
+        long long temp = i;
+        bool pass = true;
+        while (temp)
+        {
+            if (!isPrime[temp % 10])
+            {
+                pass = false;
+                break;
+            }
+            temp /= 10;
+        }
+        if (pass)
+            ++cnt;
+    }
+    // Implement logic here
+    cout << cnt << endl;
+}
+void solve2()
+{
+    int a, b;
+    cin >> a >> b;
+    int cnt = 0;
+    for (int i = a; i <= b; i++)
+    {
+        if (isPrime(i) && isDigit(i))
+            cnt++;
+    }
+    cout << cnt << endl;
+}
 class Solution
 {
 public:
     void solve()
     {
-        long long a, b;
+        int a, b;
         cin >> a >> b;
         int cnt = 0;
-        vector<bool> isPrime(b + 1, true);
-        isPrime[0] = isPrime[1] = false;
-        for (long long i = 2; i * i <= b; i++)
+        for (int i = a; i <= b; i++)
         {
-            if (!isPrime[i])
-                continue;
-            for (long long j = i * i; j <= b; j += i)
-            {
-                isPrime[j] = false;
-            }
+            if (isPrime(i) && isDigit(i))
+                cnt++;
         }
-        for (long long i = a; i <= b; i++)
-        {
-            if (!isPrime[i])
-                continue;
-            long long temp = i;
-            bool pass = true;
-            while (temp)
-            {
-                if (!isPrime[temp % 10])
-                {
-                    pass = false;
-                    break;
-                }
-                temp /= 10;
-            }
-            if (pass)
-                ++cnt;
-        }
-        // Implement logic here
         cout << cnt << endl;
     }
 };
