86973df80e63d0d903d30edd8e43984198fb9d78
Nguyen Huu Tho <tho.nguyenhuu3110@hcmut.edu.vn>
2026-08-14 15:22:53 +0700

    algo: solving problem 9: counting_divisors

---
+++ problems/tech28/L07_E9 - counting divisors/counting_divisors.cpp
@@ -0,0 +1,44 @@
+/*
+ * Problem: Tính số lượng ước số của N modulo 10^9 + 7
+ * Time Complexity: O(T) - Chỉ cần duyệt qua T dòng đầu vào.
+ * Space Complexity: O(1) - Chỉ sử dụng một vài biến cơ bản để lưu trữ tạm thời.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+const int MOD = 1000000007;
+
+class Solution
+{
+public:
+    void solve()
+    {
+        int T;
+        if (!(cin >> T))
+            return;
+
+        long long total_divisors = 1;
+
+        for (int i = 0; i < T; i++)
+        {
+            long long p, e;
+            cin >> p >> e;
+            total_divisors = (total_divisors * (e + 1)) % MOD;
+        }
+
+        cout << total_divisors << "\n";
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution solution;
+    solution.solve();
+
+    return 0;
+}
