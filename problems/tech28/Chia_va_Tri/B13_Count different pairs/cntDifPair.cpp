c7e987edc728bd91099ceb5243fd05356ed58ffc
Nguyen Huu Tho <tho.nguyenhuu3110@hcmut.edu.vn>
2026-09-05 14:37:33 +0700

    algo: cntDifPair

--- problems/tech28/Chia_va_Tri/B13_Count different pairs/cntDifPair.cpp
+++ problems/tech28/Chia_va_Tri/B13_Count different pairs/cntDifPair.cpp
@@ -5,69 +5,54 @@
  */
 
 #include <bits/stdc++.h>
-
 using namespace std;
 using ll = long long;
 ll n, k;
-ll a[1000000];
-ll tmp[1000000];
-// vector<ll> tmp;
-int dem(ll a[], ll l, ll m, ll r)
-{
+vector<ll> tmp;
 
-    ll j = m + 1;
-    ll cnt = 0;
-
-    return cnt;
-}
-void merge(ll a[], ll l, ll m, ll r)
-{
-    ll x = l;
-    ll i = l;
-    ll j = m + 1;
-    ll dem;
-
-    while (i <= m && j <= r)
-    {
-        if (a[i] <= a[j])
-        {
-            tmp[x] = a[i];
-            ++i;
-        }
-        else
-        {
-            tmp[x] = a[j];
-            ++j;
-        }
-        ++x;
-    }
-    while (i <= m)
-    {
-        tmp[x] = a[i];
-        ++i;
-        ++x;
-    }
-    while (j <= r)
-    {
-        tmp[x] = a[j];
-        ++j;
-        ++x;
-    }
-    for (ll t = l; t <= r; t++)
-    {
-        a[t] = tmp[t];
-    }
-}
-ll sapxep(ll a[], ll l, ll r)
+ll sapxep(vector<ll> &a, ll l, ll r, vector<ll> &tmp)
 {
     if (l >= r)
         return 0;
     ll m = l + (r - l) / 2;
     ll res = 0;
-    res += sapxep(a, l, m);
-    res += sapxep(a, m + 1, r);
-    res += dem(a, l, m, r);
-    merge(a, l, m, r);
+    res += sapxep(a, l, m, tmp);
+    res += sapxep(a, m + 1, r, tmp);
+    int j = m + 1;
+    for (int i = l; i <= m; i++)
+    {
+        while (j <= r && a[i] > a[j] + k)
+        {
+            j++;
+        }
+        res += (j - (m + 1)); // Cộng dồn bằng long long
+    }
+
+    int i = l;
+    j = m + 1;
+    int x = l;
+    while (i <= m && j <= r)
+    {
+        if (a[i] <= a[j])
+        {
+            tmp[x++] = a[i++];
+        }
+        else
+        {
+            tmp[x++] = a[j++];
+        }
+    }
+
+    while (i <= m)
+        tmp[x++] = a[i++];
+    while (j <= r)
+        tmp[x++] = a[j++];
+
+    for (int t = l; t <= r; t++)
+    {
+        a[t] = tmp[t];
+    }
+
     return res;
 }
 class Solution
@@ -76,11 +61,13 @@ public:
     void solve()
     {
         cin >> n >> k;
+        vector<ll> a(n);
         for (ll i = 0; i < n; i++)
         {
             cin >> a[i];
         }
-        ll ans = sapxep(a, 0, n - 1);
+        tmp.resize(n);
+        ll ans = sapxep(a, 0, n - 1, tmp);
         cout << ans;
     }
 };
